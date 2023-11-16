032475904146a1979ae86a6ff2bccc139c04bc47
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-11-16 10:04:50 +0100

    Email not found print not found

--- js/script.js
+++ js/script.js
@@ -16,22 +16,19 @@ play.addEventListener('click',function() {
     randomMachine = randomMachine.toFixed(0);
     document.getElementById("machineResult").innerHTML = randomMachine;
 
-    // Pari stampa
-    if (randomGamer == randomMachine) {
-        document.getElementById("winner").innerHTML = "Diventeremo dei Cyborg";
-    }
-
     // Se il giocatore e' più alto vince,
     if (randomGamer > randomMachine) {
         document.getElementById("winner").innerHTML = "L'umanita' non perdera mai";
     }
     // Se il giocatore e' più Basso Perde,
-     if (randomGamer < randomMachine) {
-        document.getElementById("winner").innerHTML = "La macchine conquisteranno il mondo";
+    else if (randomGamer < randomMachine) {
+    document.getElementById("winner").innerHTML = "La macchine conquisteranno il mondo";
+    }
+    // Pari stampa
+    else {
+        document.getElementById("winner").innerHTML = "Diventeremo dei Cyborg";
     }
 });
-
-
 
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (createvi una piccola lista di email),
@@ -39,8 +36,6 @@ play.addEventListener('click',function() {
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-
-
 
 const newArray = [
     "EMAIL1",
@@ -78,23 +73,22 @@ const newArray = [
 ]
 let len = newArray.length;
 
-
 // ############## CLICK ##############
 const search = document.querySelector('#search');
 search.addEventListener('click',function() {
-
-            // --------------- FOR ---------------
+let found = false ;
+        // --------------- FOR ---------------
         for (var i = 0; i < len; i++) {
             const email = document.getElementById("in-email");
 
             if (email.value == newArray[i]) {
                 console.log("Email trovato");
-                alert("Email trovato");
+                found = true ;
             }
-            else{
-                console.log("Email non trovato");
-                // alert("Email non trovato");
-            }
+        }
+        // --------------- IF ---------------
+        if (found == false) {
+            console.log("Email non trovato");
         }
 
 });
